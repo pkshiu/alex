6382124a3bfc7c9d5b1fb9d5e2d00bcb22a4a9a6
PK Shiu <pk@pkshiu.com>
2016-12-29 20:55:09 -0500

    added scroller

---
+++ scroller.py
@@ -0,0 +1,41 @@
+class Scroller(object):
+
+    def set_region(self, col, row, width):
+        """
+            Set the region that we are scrolling
+            within the lcd.
+            col/row 0-N
+        """
+        self.col = col
+        self.row = row
+        self.width = width
+        self.cp = 0
+        self.set_text('hello')
+
+    def set_text(self, text):
+        # to make things easier, padd text to guarrentee that
+        # there are width worth of spaces before and after
+        self.text = ' ' * self.width + text + ' ' * self.width
+        self.cp = 0
+        self.end_cp = len(self.text) - self.width
+        print '>%s<' % self.text
+        print self.end_cp
+
+    def scroll(self):
+        """
+        Scroll one char. return updated display text.
+        Also avail in display_text
+        """
+        if self.cp > self.end_cp:
+            self.cp = 0
+        else:
+            self.cp = self.cp + 1
+        return self.text[self.cp:self.cp+self.width]
+
+
+if __name__ == '__main__':
+    s = Scroller()
+    s.set_region(0, 0, 5)
+    s.set_text('hi')
+    for x in range(20):
+        print '>%s<' % s.scroll()
